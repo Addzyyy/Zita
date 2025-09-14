d3995c768e13444258fd58afd73d9222d9fc3c66
Adam Fulton <adam.fulton@mq.edu.au>
2025-09-14 23:54:19 +1000

    updated has conditions rule

--- src/main/kotlin/nl/utwente/processing/pmd/rules/HasConditionsRule.kt
+++ src/main/kotlin/nl/utwente/processing/pmd/rules/HasConditionsRule.kt
@@ -1,4 +1,32 @@
 package nl.utwente.processing.pmd.rules
 
-class HasConditionsRule {
+import net.sourceforge.pmd.lang.ast.Node
+import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit
+import net.sourceforge.pmd.lang.java.ast.ASTIfStatement
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule
+import net.sourceforge.pmd.RuleContext
+
+class HasConditionsRule : AbstractJavaRule() {
+
+    private var conditions = 0
+    private var firstNode: Node? = null
+
+
+    override fun visit(node: ASTCompilationUnit, data: Any?): Any? {
+        if (firstNode == null) firstNode = node
+        return super.visit(node, data)
+    }
+
+
+    override fun visit(node: ASTIfStatement, data: Any?): Any? {
+        conditions += 1
+        return  super.visit(node, data)
+    }
+
+    override fun end(ctx: RuleContext) {
+        if (conditions < 2 && firstNode != null) {
+            addViolationWithMessage(ctx, firstNode!!, message, 0, 0)
+        }
+        super.end(ctx)
+    }
 }
