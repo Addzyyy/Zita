7534dd511c41898fcd1acfb5e3dbba93c2927dfc
afulton21 <adam.fulton@mq.edu.au>
2025-09-14 23:42:22 +1000

    added rule to check for use of for each loop

---
+++ src/main/kotlin/nl/utwente/processing/pmd/rules/HasForEachLoopRule.kt
@@ -0,0 +1,41 @@
+package nl.utwente.processing.pmd.rules
+
+import net.sourceforge.pmd.RuleContext
+import net.sourceforge.pmd.lang.java.ast.ASTForStatement
+import net.sourceforge.pmd.lang.java.ast.ASTExpression
+import net.sourceforge.pmd.lang.java.ast.ASTForInit
+import net.sourceforge.pmd.lang.java.ast.ASTForUpdate
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule
+
+/**
+ * Rule to detect use of enhanced for-each loops like: for (Type item : collection)
+ */
+class HasForEachLoopRule : AbstractJavaRule() {
+
+    private var firstForEachNode: ASTForStatement? = null
+
+    override fun visit(node: ASTForStatement, data: Any?): Any? {
+        val hasExpression = node.getFirstDescendantOfType(ASTExpression::class.java) != null
+        val hasNoInit = node.getFirstChildOfType(ASTForInit::class.java) == null
+        val hasNoUpdate = node.getFirstChildOfType(ASTForUpdate::class.java) == null
+
+        if (hasExpression && hasNoInit && hasNoUpdate && firstForEachNode == null) {
+            firstForEachNode = node
+        }
+
+        return super.visit(node, data)
+    }
+
+    override fun end(ctx: RuleContext?) {
+        if (ctx != null && firstForEachNode != null) {
+            addViolationWithMessage(
+                ctx,
+                firstForEachNode,
+                "Submission uses a for-each loop. Consider asking the student about its purpose.",
+                firstForEachNode!!.beginLine,
+                firstForEachNode!!.endLine
+            )
+        }
+        super.end(ctx)
+    }
+}
