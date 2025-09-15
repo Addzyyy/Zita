a2f9a58434be2f5e0a18fccd59b63f3c810185cc
Adam Fulton <adam.fulton@mq.edu.au>
2025-09-15 22:33:03 +1000

    updated HasUserDefinedClass Rule

--- src/main/kotlin/nl/utwente/processing/pmd/rules/HasUserDefinedConstructor.kt
+++ src/main/kotlin/nl/utwente/processing/pmd/rules/HasUserDefinedConstructor.kt
@@ -1,4 +1,44 @@
 package nl.utwente.processing.pmd.rules
 
-class HasUserDefinedConsturctor {
+import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration
+import net.sourceforge.pmd.lang.java.ast.ASTConstructorDeclaration
+import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule
+
+class HasUserDefinedConstructor: AbstractJavaRule() {
+
+    private var outerClass: ASTClassOrInterfaceDeclaration? = null
+
+    override fun visit(node: ASTClassOrInterfaceDeclaration?, data: Any?): Any? {
+        if (node == null) return data
+
+        if (outerClass == null) {
+            // First class is assumed to be the top-level wrapper
+            outerClass = node
+            return super.visit(node, data)
+        }
+
+        // Skip interfaces or top-level class itself
+        if (node == outerClass || node.isInterface) {
+            return super.visit(node, data)
+        }
+
+        val hasConstructor = node.findDescendantsOfType(ASTConstructorDeclaration::class.java)
+            .any {it.image == null || it.image == node.image}
+
+        // Check for presence of constructor(s)
+
+
+        if (!hasConstructor) {
+            addViolationWithMessage(
+                data,
+                node,
+              message,
+                0,
+                0
+            )
+        }
+
+
+        return super.visit(node, data)
+    }
 }
